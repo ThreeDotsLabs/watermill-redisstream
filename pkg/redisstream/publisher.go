3c809bab4b9524258c26ee51ef7fcfe23aec0858
Vitor Arins <v.arins@sensysgatso.com>
2023-08-23 15:37:31 +0200

    Add 'DefaultMaxlen' option to 'PublisherConfig'

--- pkg/redisstream/publisher.go
+++ pkg/redisstream/publisher.go
@@ -40,9 +40,10 @@ func NewPublisher(config PublisherConfig, logger watermill.LoggerAdapter) (*Publ
 }
 
 type PublisherConfig struct {
-	Client     redis.UniversalClient
-	Marshaller Marshaller
-	Maxlens    map[string]int64
+	Client        redis.UniversalClient
+	Marshaller    Marshaller
+	Maxlens       map[string]int64
+	DefaultMaxlen int64
 }
 
 func (c *PublisherConfig) setDefaults() {
@@ -58,7 +59,7 @@ func (c *PublisherConfig) Validate() error {
 	for topic, maxlen := range c.Maxlens {
 		if maxlen < 0 {
 			// zero maxlen stream indicates unlimited stream length
-			c.Maxlens[topic] = 0
+			c.Maxlens[topic] = c.DefaultMaxlen
 		}
 	}
 	return nil
@@ -87,7 +88,7 @@ func (p *Publisher) Publish(topic string, msgs ...*message.Message) error {
 
 		maxlen, ok := p.config.Maxlens[topic]
 		if !ok {
-			maxlen = 0
+			maxlen = p.config.DefaultMaxlen
 		}
 
 		id, err := p.client.XAdd(context.Background(), &redis.XAddArgs{
